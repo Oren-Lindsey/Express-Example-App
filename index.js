34d4b58d059527093dc0c6dbd1d73320da4e2476
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-03-10 18:19:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/html/index.html');
 })
 
+app.get('/api', (req, res) => {
+  res.send({"hello":"world"})
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}.`)
 })
